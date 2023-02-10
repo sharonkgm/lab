938c5b8c9582fc3529b5d1c0b314ecfe64b42386
sharon <sharontkkgm@gmail.com>
2023-02-11 03:37:17 +0530

    modified

---
+++ 1st sem/lab/cycle_1/string_matching.py
@@ -0,0 +1,6 @@
+string1 = input("enter a string :")
+string2 = input("enter next string :")
+if string1 == string2:
+    print("two strings are matching")
+else:
+     print("two strings are not matching")
